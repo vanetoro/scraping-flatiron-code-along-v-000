9d0a576852c864319590a7c889b678448f0ddff3
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 17:53:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,12 +25,12 @@ class Scraper
   
   def make_courses
       self.get_courses.each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end 
-
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end 
+  end  
 end
 
 Scraper.new.get_page
